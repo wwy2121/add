38a9c27bee2c5afd18c9b65d7001033ae3243253
吴问应 <wuwenying@theduapp.com>
2019-09-18 09:41:35 +0800

    测试下

---
+++ test1/src/test1.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wwy2121
+ * Date: 2019/9/18
+ * Time: 9:36
+ */
+
+namespace Test1\my;
+class test1{
+    public function add() {
+        echo time();
+    }
+}
