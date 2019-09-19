91b18b692c92b9b8d060af2c489a7af2a27dba9e
吴问应 <wuwenying@theduapp.com>
2019-09-19 09:42:48 +0800

    1

---
+++ group3/src/info.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wwy2121
+ * Date: 2019/9/19
+ * Time: 9:39
+ */
+
+namespace info;
+
+class info {
+    public function getInfo() {
+        echo "info3";
+    }
+}
